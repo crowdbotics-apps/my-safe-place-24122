ccbf6012e85ec0c33ffdd258a6c518f3c099b961
Crowdbotics <team@crowdbotics.com>
2021-01-28 18:22:24 +0000

    remove package blueprint-react-native-notifications-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -12,7 +12,6 @@ export const installed_blueprints = [
 { name: 'SignIn2197844', human_name: 'Sign In 2', access_route: 'SignIn2197844'},
 { name: 'UserProfile197831', human_name: 'User Profile', access_route: 'UserProfile197831'},
 { name: 'Tutorial197830', human_name: 'Tutorial', access_route: 'Tutorial197830', icon: 'question-circle'},
-{ name: 'NotificationList197802', human_name: 'Notification List', access_route: 'NotificationList197802'},
 { name: 'Settings197801', human_name: 'Settings', access_route: 'Settings197801'},
 { name: 'UserProfile197791', human_name: 'User Profile', access_route: 'UserProfile197791'},
 
